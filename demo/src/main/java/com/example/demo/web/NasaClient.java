4d756e8ced3c8303a9bc2d8029dc306981c62e2f
karoool <jensonkarol7@gmail.com>
2024-12-20 12:57:19 +0100

    Wiremock

--- demo/src/main/java/com/example/demo/web/NasaClient.java
+++ demo/src/main/java/com/example/demo/web/NasaClient.java
@@ -3,7 +3,7 @@ package com.example.demo.web;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "nasa-client", url = "https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json")
+@FeignClient(name = "nasa-client", url = "${nasa-client.url}")
 public interface NasaClient {
 
     @GetMapping
--- demo/src/test/java/com/example/demo/domain/IntegrationTest.java
+++ demo/src/test/java/com/example/demo/domain/IntegrationTest.java
@@ -3,7 +3,9 @@ package com.example.demo.domain;
 import com.example.demo.config.TestInitDatabase;
 import com.example.demo.model.MarsDailyWeather;
 import com.example.demo.repo.MarsRepo;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -11,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
@@ -20,6 +23,9 @@ public class IntegrationTest {
 
     @Autowired
     private MarsRepo marsRepo;
+
+    @RegisterExtension
+    static WireMockExtension wireMock = WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
     @Test
     void shouldFindMarsDailyWeatherBySol(){
