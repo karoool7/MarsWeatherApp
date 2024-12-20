5fdbbb0d166a82d333a424cf0ef4f4d2b3043b91
karoool <jensonkarol7@gmail.com>
2024-12-20 10:53:00 +0100

    klasa TestInitDatabase + application-test.yml

--- demo/src/test/java/com/example/demo/domain/IntegrationTest.java
+++ demo/src/test/java/com/example/demo/domain/IntegrationTest.java
@@ -1,43 +1,29 @@
 package com.example.demo.domain;
 
+import com.example.demo.config.TestInitDatabase;
 import com.example.demo.model.MarsDailyWeather;
 import com.example.demo.repo.MarsRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@Import(TestInitDatabase.class)
 public class IntegrationTest {
 
     @Autowired
     private MarsRepo marsRepo;
 
     @Test
-    void testSaveAndFindSol(){
-        // given
-        MarsDailyWeather sol = new MarsDailyWeather(
-                null,
-                "2024-11-01",
-                7777,
-                "33",
-                "Month 2",
-                "-30",
-                "5",
-                "555",
-                "High",
-                "Sunny",
-                "08:33",
-                "17:33",
-                "Moderate",
-                "-90",
-                "-1"
-        );
+    void shouldFindMarsDailyWeatherBySol(){
         //when
-        marsRepo.save(sol);
         Optional<MarsDailyWeather> solFromDb = marsRepo.findBySol(7777);
         //then
         assertThat(solFromDb).isPresent()
