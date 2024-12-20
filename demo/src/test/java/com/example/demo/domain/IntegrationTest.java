b7c1d32037df5b0de1cd6806305a1453cd2ddf82
karoool <jensonkarol7@gmail.com>
2024-12-20 21:59:53 +0100

    configureProperties

--- demo/src/test/java/com/example/demo/domain/IntegrationTest.java
+++ demo/src/test/java/com/example/demo/domain/IntegrationTest.java
@@ -4,18 +4,22 @@ import com.example.demo.config.TestInitDatabase;
 import com.example.demo.model.MarsDailyWeather;
 import com.example.demo.repo.MarsRepo;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @ActiveProfiles("test")
 @SpringBootTest
 @Import(TestInitDatabase.class)
@@ -27,8 +31,13 @@ public class IntegrationTest {
     @RegisterExtension
     static WireMockExtension wireMock = WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("nasa-client.url", () -> "http://localhost:" + wireMock.getPort());
+    }
+
     @Test
-    void shouldFindMarsDailyWeatherBySol(){
+    void shouldFindMarsDailyWeatherBySol() {
         //when
         Optional<MarsDailyWeather> solFromDb = marsRepo.findBySol(7777);
         //then
@@ -36,5 +45,6 @@ public class IntegrationTest {
                 .get()
                 .extracting(MarsDailyWeather::getSol)
                 .isEqualTo(7777);
+        log.info("Wielkość bazy danych {}", marsRepo.count());
     }
 }
