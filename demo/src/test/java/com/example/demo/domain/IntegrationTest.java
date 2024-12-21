790a2b872a6c7d8865eeca08d6405884e0b936e4
karoool <jensonkarol7@gmail.com>
2024-12-21 14:53:52 +0100

    testGetWeatherFromOpenFeign

--- demo/src/test/java/com/example/demo/domain/IntegrationTest.java
+++ demo/src/test/java/com/example/demo/domain/IntegrationTest.java
@@ -3,6 +3,11 @@ package com.example.demo.domain;
 import com.example.demo.config.TestInitDatabase;
 import com.example.demo.model.MarsDailyWeather;
 import com.example.demo.repo.MarsRepo;
+import com.example.demo.web.MarsWeatherDetailsDto;
+import com.example.demo.web.MarsWeatherDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -14,10 +19,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Slf4j
 @ActiveProfiles("test")
@@ -27,9 +36,12 @@ public class IntegrationTest {
 
     @Autowired
     private MarsRepo marsRepo;
+    @Autowired
+    private MarsService marsService;
 
     @RegisterExtension
-    static WireMockExtension wireMock = WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
+    static WireMockExtension wireMock =
+            WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
@@ -46,5 +58,51 @@ public class IntegrationTest {
                 .extracting(MarsDailyWeather::getSol)
                 .isEqualTo(7777);
         log.info("Wielkość bazy danych {}", marsRepo.count());
+    }
+
+    @Test
+    void testGetWeatherFromOpenFeign() throws JsonProcessingException {
+        //given
+        String weatherDtoAsJson = createMarsWeatherDtoAsJson();
+        WireMock.configureFor("localhost", wireMock.getPort());
+        stubFor(get(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(weatherDtoAsJson)));
+        //when
+        MarsWeatherDto response = marsService.fetchMarsWeatherFromNasa();
+        //then
+        assertAll(
+                () -> assertThat(response).isNotNull(),
+                () -> assertThat(response.soles().size()).isEqualTo(1),
+                () -> assertThat(response.soles().get(0).minTemp()).isEqualTo("-30"),
+                () -> assertThat(response.soles().get(0).pressure()).isEqualTo("555")
+        );
+    }
+
+    private static String createMarsWeatherDtoAsJson() throws JsonProcessingException {
+        List<MarsWeatherDetailsDto> detailsDtos = new ArrayList<>();
+        MarsWeatherDetailsDto dto = new MarsWeatherDetailsDto(
+                "2024-11-01",
+                "7777",
+                "33",
+                "Month 2",
+                "-30",
+                "5",
+                "555",
+                "High",
+                "Sunny",
+                "08:33",
+                "17:33",
+                "Moderate",
+                "-90",
+                "-1"
+        );
+        detailsDtos.add(dto);
+        MarsWeatherDto weatherDto = new MarsWeatherDto(detailsDtos);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String weatherDtoAsJson = objectMapper.writeValueAsString(weatherDto);
+        return weatherDtoAsJson;
     }
 }
